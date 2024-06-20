f24a9904496fb2af4bd5c19361d8c66f606f8ec9
Mohd Tauseef Ansari <mohdtauseefansari34@gmail.com>
2024-06-21 00:01:29 +0530

    Solved the issue of not getting same background image dynamically while searching for the city & also Solved the issue of not getting weather data while fetching it from the API (Main Issues are resolved)

--- script.js
+++ script.js
@@ -1,90 +1,112 @@
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'c4c24927ccmsha9cafa99ebd0ec3p172fbcjsnc4552afe9eab',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-};
+// for bg image
+const client_id = "_AE7sCO8tWFmAiWkdlve6pwwCOhJMeo8lmEBRQhSgls";
+
+// for Weathr data
+const API_key = "bff141d1f5b5a5b33765029b0ce71b58";
+
 const getWeather = (city) => {
-    // Show loading animation
-    document.getElementById("loading").style.display = "block";
+  // Show loading animation
+  document.getElementById("loading").style.display = "block";
 
-    //Getting City name from search
-    cityName.innerHTML = city
-    // Getting random image from unsplash and changing the background
-    document.body.style.backgroundImage = "url(https://source.unsplash.com/random/?"+city+")";
-    // Getting Weather data from weather api
+  //Getting City name from search
+  cityName.innerHTML = city;
 
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-        .then(response => response.json())
-        .then((response) => {
-            console.log(response)
-            let sunRise = response.sunrise
-            let sunriseTime = new Date(sunRise * 1000);
-            let sunSet = response.sunset
-            let sunsetTime = new Date(sunSet * 1000);
+  // Getting random image from unsplash and changing the background
+  fetch(
+    `https://api.unsplash.com/search/photos?client_id=${client_id}&query=${city}`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
 
-            //Updating weather on weather app
-            temp.innerHTML = response.temp
-            max_temp.innerHTML = response.max_temp
-            min_temp.innerHTML = response.min_temp
-            humidity.innerHTML = response.humidity
-            feels_like.innerHTML = response.feels_like
-            wind_speed.innerHTML = response.wind_speed
-            wind_degrees.innerHTML = response.wind_degrees
-            sunrise.innerHTML = sunriseTime.toLocaleTimeString()
-            sunset.innerHTML = sunsetTime.toLocaleTimeString()
+      // For getting random image of th searched city
+      let x = Math.floor(Math.random() * 10 + 1);
 
-            // Hide loading animation after loading is complete
-            document.getElementById("loading").style.display = "none";
-        })
-        .catch(err => {
-            console.error(err);
-            // Hide loading animation in case of an error
-            document.getElementById("loading").style.display = "none";
-        });
-}
+      // for chnaging the background image dynamically same as the city which is searchd
+      document.body.style.backgroundImage = `url(${response.results[x].urls.raw})`;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  // Getting Weather data from weather api
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_key}`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      let sunRise = response.sys.sunrise;
+      let sunriseTime = new Date(sunRise * 1000);
+      let sunSet = response.sys.sunset;
+      let sunsetTime = new Date(sunSet * 1000);
+
+      //Updating weather on weather app
+      temp.innerHTML = response.main.temp;
+      max_temp.innerHTML = response.main.temp_max;
+      min_temp.innerHTML = response.main.temp_min;
+      humidity.innerHTML = response.main.humidity;
+      feels_like.innerHTML = response.main.feels_like;
+      wind_speed.innerHTML = response.wind.speed;
+      wind_degrees.innerHTML = response.wind.deg;
+      sunrise.innerHTML = sunriseTime.toLocaleTimeString();
+      sunset.innerHTML = sunsetTime.toLocaleTimeString();
+
+      // Hide loading animation after loading is complete
+      document.getElementById("loading").style.display = "none";
+    })
+    .catch((err) => {
+      console.error(err);
+      // Hide loading animation in case of an error
+      document.getElementById("loading").style.display = "none";
+    });
+};
 
 // Getting city name by user
 
 submit.addEventListener("click", (e) => {
-    e.preventDefault()
-    getWeather(city.value)
-})
+  e.preventDefault();
+  getWeather(city.value);
+});
 
 //Calling the function
-getWeather("Delhi")
+getWeather("Delhi");
 
 const getWeatherForOtherCity = async () => {
+  //Getting Other cities name from HTML File
+  let items = document.getElementsByClassName("otherCity");
+  let otherCity = [].map.call(items, (otherCity) => otherCity.textContent);
+  console.log(otherCity);
 
-    //Getting Other cities name from HTML File
-    let items = document.getElementsByClassName("otherCity");
-    let otherCity = [].map.call(items, otherCity => otherCity.textContent);
-    console.log(otherCity);
+  // Gettitng Weather of Other Cities
 
-    // Gettitng Weather of Other Cities
+  for (let i = 0; i < 5; i++) {
+    const element = otherCity[i];
+    console.log(element);
+    let weatherData = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${element}&appid=${API_key}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .catch((err) => console.error(err));
+    let sunRise = weatherData.sys.sunrise;
+    let sunriseTime = new Date(sunRise * 1000).toLocaleTimeString();
+    let sunSet = weatherData.sys.sunset;
+    let sunsetTime = new Date(sunSet * 1000).toLocaleTimeString();
 
-    for (let i = 0; i < 5; i++) {
-        const element = otherCity[i];
-        console.log(element);
-        let weatherData = await fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + element, options)
-            .then(response => response.json())
-            .then((response) => {
-                console.log(response);
-                return response;
-            })
-            .catch(err => console.error(err));
-        let sunRise = weatherData.sunrise
-        let sunriseTime = new Date(sunRise * 1000).toLocaleTimeString();
-        let sunSet = weatherData.sunset
-        let sunsetTime = new Date(sunSet * 1000).toLocaleTimeString();
-        
-        //Updating weather on weather app
-        document.getElementById(`${element}_temp`).innerHTML = weatherData.temp
-        document.getElementById(`${element}_humidity`).innerHTML = weatherData.humidity
-        document.getElementById(`${element}_windspeed`).innerHTML = weatherData.wind_speed
-        document.getElementById(`${element}_sunrise`).innerHTML = sunriseTime
-        document.getElementById(`${element}_sunset`).innerHTML = sunsetTime
-    }
-}
+    //Updating weather on weather app
+    document.getElementById(`${element}_temp`).innerHTML =
+      weatherData.main.temp;
+    document.getElementById(`${element}_humidity`).innerHTML =
+      weatherData.main.humidity;
+    document.getElementById(`${element}_windspeed`).innerHTML =
+      weatherData.wind.speed;
+    document.getElementById(`${element}_sunrise`).innerHTML = sunriseTime;
+    document.getElementById(`${element}_sunset`).innerHTML = sunsetTime;
+  }
+};
 getWeatherForOtherCity();
